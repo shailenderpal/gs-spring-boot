cfedd5659a255a42486420afec588f476288a4ba
shailenderpal <shailenderpal@users.noreply.github.com>
2020-05-10 19:40:36 +0530

    greeting message changed.

--- app/src/test/java/hello/HelloControllerTest.java
+++ app/src/test/java/hello/HelloControllerTest.java
@@ -26,6 +26,8 @@ public class HelloControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello from Spring Boot!")));
+                .andExpect(content().string(equalTo("Greetings from Srini!")));
+        //Greetings from Srini!
+        //Hello from Spring Boot!
     }
 }
